e023f9b57e13823f3bc290e3eb5cdf6bff1df4be
Beginerofcod <amish.game.pro@gmail.com>
2020-09-12 10:44:02 +0545

    lab 2

---
+++ Mean and Median/Mean and median/Mean and median/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Mean_and_median
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 10, 2, 1, 4, 2, 50 };
+            Array.Sort(list);
+            double sum = 0;
+            foreach (double element in list)
+            {
+                sum += element;
+            }
+            double mean = sum / list.Length;
+            double median = 0;
+            if ((list.Length % 2) == 0)
+            {
+                median = (list[(list.Length / 2) - 1] + list[(list.Length / 2)]) / 2.0;
+            }
+            else
+            {
+                median = (list[(list.Length / 2)]);
+            }
+            Console.WriteLine($"Mean of given list = {mean}");
+            Console.WriteLine($"Median of given list = {median}");
+        }
+    }
+}
---
+++ Multiple of table/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Enter any number:");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Until what value do you want the multiplication to go: ");
+            int multiplier = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= multiplier; i++)
+            {
+                Console.WriteLine($"{number} * {i} = {number * i}");
+            }
+        }
+    }
+}
